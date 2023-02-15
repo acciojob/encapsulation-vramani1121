411c257be75f1bb6af23d3472bd5cf3b9975b0ed
vivek ramani <92964991+vramani1121@users.noreply.github.com>
2023-02-15 14:06:48 +0530

    first

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String []args) {
+        RWOnly obj = new RWOnly();
+
+        obj.setName("vivek");
+
+        System.out.println(obj.getName());
+    }
 }
